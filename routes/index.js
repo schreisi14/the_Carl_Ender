e39f866f4c86d05f25e2d2f49b29e8836972107e
Heli Weithaler <helmuth.weithaler@gmail.com>
2015-12-16 00:26:13 +0100

    Added LogIn-v0.1, added comments to server.js, updated Readme

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
-router.post('/login', function(req,res){
-	//TODO - LOGIN
-	res.redirect('content/index.html');
-	res.end("Login");
+//Rout to Login
+router.post('/login', 
+	 passport.authenticate('local', { 	successRedirect: 'content/index.html',
+                                   		failureRedirect: '/login',
+                                   		failureFlash: true 
+                                   	}));
+
+//Rout when LoginFails
+router.get('/login',function(req,res){
+	res.end("ungültig");
 });
 
+
+//Route to Logout - Unused atm
 router.all('/logout',function(req,res){
 	//TODO - LOGOUT
 	res.end("Logout");
 });
 
 //Route for everthing else
-
 router.all('*', express.static('public/'));
 
 

c12cec7f5e58eb38a82047ce904bae9eb3275976
Heli Weithaler <helmuth.weithaler@gmail.com>
2016-01-14 19:39:28 +0100

    Switched from Bcrypt to Bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt');
+var bcrypt   = require('bcryptjs');
 
 // Defines a Schema for the User-Model
 var userSchema = mongoose.Schema({

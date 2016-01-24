099bcedbf8e507fd4f8561839a4cf741e63a2c1a
Heli Weithaler <helmuth.weithaler@gmail.com>
2016-01-24 15:32:54 +0100

    Date working

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(flash());
 //Set View Engine / View Render
 var hbs = exphbs.create({
   extname: '.hbs',
-  helpers: {'dataFormat': function(date){return date.toDateString();}},
+  helpers: {'dataFormat': function(date){return date.getDate() + "." + (date.getMonth()+1) + "." + date.getFullYear();}},
 });
 app.engine('.hbs', hbs.engine);  //Register Handlebars as Renderengine
 app.set('view engine', '.hbs');                 //.hbs Files in the views folger are now rendered with Handlebars
